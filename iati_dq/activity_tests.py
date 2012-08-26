698112f91664d27cd68a395fcc4dfb1185505b5f
Mark Brough <mark.brough@publishwhatyoufund.org>
2012-08-26 12:10:00 +0100

    Updated activity tests

--- iati_dq/activity_tests.py
+++ iati_dq/activity_tests.py
@@ -1,23 +1,32 @@
 # provide variables from xpath API
 
-def test_check_date(activity):
+def activity_date_exists(activity):
+    """
+    Activity Date ISO date exists
+    """
+    # Should also check whether it's in the format YYYY-MM-DD
     thedate = activity.find('activity-date').get('iso-date')
     if (thedate is None):
-	    return False
+        return False
     else:
         return True
 
-def test_title(activity):
+def title_exists(activity):
+    """
+    Title exists
+    """
     thetitle = activity.find('title').text
-    
     if (thetitle is None):
-	    return False
+        return False
     else:
         return True
 
-def test_title_size(activity):
+def title_greater_than_10_characters(activity):
+    """
+    Title is greater than 10 characters
+    """
     thetitle = activity.find('title').text
-    if ((thetitle is not None) and (len(thetitle)>20)):
+    if ((thetitle is not None) and (len(thetitle)>10)):
 	    return True
     else:
         return False
