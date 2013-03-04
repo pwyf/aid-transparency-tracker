14d23d735912eea6773119fa9bd81e91f8b4dca4
Martin Keegan <martin@no.ucant.org>
2013-03-04 16:42:18 +0000

    generalise

--- unittests/test_runtests.py
+++ unittests/test_runtests.py
@@ -9,17 +9,23 @@ import iatidataquality
 from iatidataquality import db
 import lxml.etree
 
-def test_checktitle():
+def create_tst(name):
     test = iatidataquality.models.Test()
-    test.name = "title/text() exists?"
+    test.name = name
     test.active = True
-    test.description = "Does title exist?"
-    test.test_group = "title"
+    test.description = "Test FN"
+    test.test_group = "???"
     test.test_level=1
     db.session.add(test)
     db.session.commit()
-    print "Wrote to DB, ID is",test.id
+    return test
 
+def remove_tst(test):
+    db.session.delete(test)
+    db.session.commit()
+
+def check_against_files(test_str):
+    test = create_tst(test_str)
     from iatidataquality.dqparsetests import test_functions as tf
     test_functions = tf()
 
@@ -31,10 +37,19 @@ def test_checktitle():
     def check_data_file(data_file, expected_result):
         parse_tree = lxml.etree.parse(os.path.join(current, data_file))
         activity = parse_tree.find('iati-activity')
-        assert test_functions[test.id](activity) == expected_result
+        observed = test_functions[test.id](activity)
+        print observed, expected_result
+        assert observed == expected_result
 
     [ check_data_file(data_file, expected_result)
       for data_file, expected_result in data_files ]
 
-    db.session.delete(test)
-    db.session.commit()
+    remove_tst(test)
+
+def test_checktitle():
+    tests = [
+        "title/text() exists?",
+        "description/text() exists?"
+        ]
+
+    [ check_against_files(test) for test in tests ]
