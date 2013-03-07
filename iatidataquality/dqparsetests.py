dee3ca14ee307c8d22970f62a6180e92ffd54890
Martin Keegan <martin@no.ucant.org>
2013-03-07 21:50:51 +0000

    fix transaction

--- iatidataquality/dqparsetests.py
+++ iatidataquality/dqparsetests.py
@@ -1,9 +1,14 @@
 import iatidataquality.models as models
 import foxpath
 
+from iatidataquality import db
+
 def get_active_tests():
     for test in models.Test.query.filter(models.Test.active == True).all():
         yield test
 
 def test_functions():
-    return foxpath.generate_test_functions(get_active_tests())
+    try:
+        return foxpath.generate_test_functions(get_active_tests())
+    finally:
+        db.session.commit()
