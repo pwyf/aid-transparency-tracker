c485d54d6f3866dcbd390798d404dedff437cf9c
Martin Keegan <martin@no.ucant.org>
2013-03-08 01:29:22 +0000

    add tests for aggregation function

---
+++ unittests/test_aggregationfn.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import csv
+
+current = os.path.dirname(os.path.abspath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import iatidataquality
+import iatidataquality.dqfunctions
+
+import nose
+import nose.tools
+
+import json
+
+def setup_func():
+    pass
+
+def teardown_func():
+    pass
+
+def check_aggregation(config):
+    suffix, mode = config
+
+    with file('unittests/artefacts/input-%s.json' % suffix) as f:
+        data = json.load(f)
+
+    with file('unittests/artefacts/output-%s.json' % suffix) as f:
+        expected = f.read()
+
+    observed = iatidataquality.dqfunctions._agr_results(
+        data,
+        conditions=None,
+        mode=mode
+        )
+
+    assert json.dumps(observed, indent=2) == expected
+
+@nose.with_setup(setup_func, teardown_func)
+def test_aggregation_fn():
+    configs = [
+        ("publisher", "publisher"),
+        ("package", None)
+        ]
+
+    for cfg in configs:
+        yield (check_aggregation, cfg)
