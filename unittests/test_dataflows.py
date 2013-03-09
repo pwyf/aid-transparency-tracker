fbeb27556a98f3870a1f83c1aa1c6506dc358742
Martin Keegan <martin@no.ucant.org>
2013-03-09 12:26:55 +0000

    add test for ckan lookups

---
+++ unittests/test_dataflows.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import csv
+
+current = os.path.dirname(os.path.abspath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+import iatidq
+
+import nose
+import nose.tools
+
+import json
+import uuid
+
+import iatidq.dqregistry
+
+from iatidq import db
+from iatidq import models
+
+def setup_func():
+    for pkg in models.Package.query.filter_by(
+        package_name="testbank-tz").all():
+        db.session.delete(pkg)
+    db.session.commit()
+
+def teardown_func():
+    for pkg in models.Package.query.filter_by(
+        package_name="testbank-tz").all():
+        db.session.delete(pkg)
+    db.session.commit()
+
+@nose.with_setup(setup_func, teardown_func)
+def test_change_title():
+    with file('unittests/artefacts/pkgdata-worldbank-tz.json') as f:
+        package = json.load(f)
+    iatidq.dqregistry.refresh_package(package)
+
+    pkg = models.Package.query.filter_by(
+        package_name=package['name']).first()
+    assert pkg is not None
+
+    new_title = "Retitled package"
+    assert new_title != pkg.package_title
+
+    package['title'] = new_title
+    iatidq.dqregistry.refresh_package(package)
+    
+    pkg = models.Package.query.filter_by(
+        package_name=package['name']).first()
+
+    assert new_title == pkg.package_title
