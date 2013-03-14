6d3cb11203210abbbd8eb5ca8a508f506aa96b92
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-03-14 21:15:54 +0000

    Tidy up

--- tests/file_tests.py
+++
@@ -1,6 +0,0 @@
-def test_unique_identifiers(activities):
-    context = activities.findall("//iati-activity//iati-identifier")
-    if (context is None):
-	return False
-    else:
-        return True
