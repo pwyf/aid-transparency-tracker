7dcb4b6941dccd4ba8f20600bbaa460c66f67da0
Martin Keegan <martin@no.ucant.org>
2013-04-16 15:57:20 +0100

    add main test test

--- unittests/test_pipeline.py
+++ unittests/test_pipeline.py
@@ -11,6 +11,10 @@ sys.path.append(parent)
 import iatidq
 import iatidq.dqparsetests
 import iatidq.dqregistry
+import iatidq.dqimporttests
+import iatidq.dqindicators
+import iatidq.test_queue
+import iatidq.dqcodelists
 
 from iatidq import db
 import lxml.etree
@@ -28,6 +32,14 @@ def setup_func():
 def teardown_func():
     pass
 
+def get_packagegroups_by_name(name):
+    return iatidq.models.PackageGroup.query.filter(
+        PackageGroup.name==name).all()
+
+def get_packages_by_name(name):
+    return iatidq.models.Package.query.filter(
+        Package.package_name==name).all()
+
 @nose.with_setup(setup_func, teardown_func)
 def test_refresh():
     publisher = 'dfid'
@@ -35,21 +47,82 @@ def test_refresh():
     package_name = '-'.join([publisher, country])
 
     # check there's nothing in the db
-    pgs = iatidq.models.PackageGroup.query.filter(PackageGroup.name==publisher).all()
+    pgs = get_packagegroups_by_name(publisher)
     assert len(pgs) == 0
-    pkgs = iatidq.models.PackageGroup.query.filter(Package.package_name==package_name).all()
+    pkgs = get_packages_by_name(package_name)
     assert len(pkgs) == 0
 
     # do refresh
     iatidq.dqregistry.refresh_package_by_name(package_name)
 
     # check there's something in the db
-    pgs = iatidq.models.PackageGroup.query.filter(PackageGroup.name==publisher).all()
+    pgs = get_packagegroups_by_name(publisher)
     assert len(pgs) == 1
     pg = pgs[0]
     assert pg.name == publisher
 
-    pkgs = iatidq.models.Package.query.filter(Package.package_name==package_name).all()
+    pkgs = get_packages_by_name(package_name)
     assert len(pkgs) == 1
     pkg = pkgs[0]
     assert pkg.package_name == package_name
+
+@nose.with_setup(setup_func, teardown_func)
+def test_example_tests():
+    publisher = 'worldbank'
+    country = 'tz'
+    package_name = '-'.join([publisher, country])
+
+    # check there's nothing in the db
+    pgs = get_packagegroups_by_name(publisher)
+    assert len(pgs) == 0
+    pkgs = get_packages_by_name(package_name)
+    assert len(pkgs) == 0
+
+    # do refresh
+    iatidq.dqregistry.refresh_package_by_name(package_name)
+
+    iatidq.dqimporttests.hardcodedTests()
+
+    # load foxpath tests
+    os.chdir(parent)
+    iatidq.dqimporttests.importTestsFromFile(
+        filename="tests/iati2foxpath_tests.csv")
+    print "Importing indicators"
+    iatidq.dqindicators.importIndicatorsFromFile(
+        "test_pwyf2013",
+        "tests/iati2foxpath_tests.csv")
+
+    iatidq.dqindicators.importIndicatorDescriptionsFromFile("pwyf2013", 
+                                                            "tests/indicators.csv")
+    print "Importing codelists"
+    iatidq.dqcodelists.importCodelists()
+    print "Refreshing package data from Registry"
+    dqregistry.refresh_packages()
+
+
+
+ 
+    from iatidq.dqparsetests import test_functions as tf
+    test_functions = tf()
+    from iatidq import dqcodelists
+    codelists = dqcodelists.generateCodelists()
+
+#    for t in iatidq.models.Test.query.all():
+#        print t
+
+    from iatidq.testrun import start_new_testrun
+    runtime = start_new_testrun()
+
+    results = iatidq.models.Result.query.all()
+    assert len(results) == 0
+
+    pkg = get_packages_by_name('worldbank-tz')[0]
+
+    iatidq.test_queue.check_file(test_functions, 
+               codelists,
+               "pkgdata-worldbank-tz.json",
+               runtime.id,
+               pkg.id,
+               context=None)
+
+    assert len(results) > 0
