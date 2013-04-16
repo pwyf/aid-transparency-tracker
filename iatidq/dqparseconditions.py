c52cb0f91f626dd415ed45278aea90538192a631
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-04-16 21:39:38 +0100

    Rename publisher to organisation

--- iatidq/dqparseconditions.py
+++ iatidq/dqparseconditions.py
@@ -31,22 +31,22 @@ def add_partial(regex):
         return fn
     return append_to_mappings
 
-def parsePC(publisher_structures):
+def parsePC(organisation_structures):
 
-    def publisher_and_test_level(groups):
+    def organisation_and_test_level(groups):
         like = '%' + groups[1] + '%'
-        publisher = models.PackageGroup.query.filter_by(name=groups[0]).first()
+        organisation = models.Organisation.query.filter_by(organisation_name=groups[0]).first()
         tests = db.session.query(
             models.Test.id, 
             models.Test.name, 
             models.Test.description
             ).filter(models.Test.name.like(like)).all()
-        return publisher, tests
+        return organisation, tests
 
     @add_partial('(\S*) does not use (\S*) at activity level')
     def doesnt_use_at_activity_level(activity, groups):
-        publisher, tests = publisher_and_test_level(groups)
-        return {'publisher':publisher, 
+        organisation, tests = organisation_and_test_level(groups)
+        return {'organisation':organisation, 
                 'tests': tests, 
                 'operation': 0, 
                 'condition': 'activity level', 
@@ -54,8 +54,8 @@ def parsePC(publisher_structures):
 
     @add_partial('(\S*) does not use (\S*) at activity hierarchy (\d*)')
     def doesnt_use_at_activity_hierarchy(activity, groups):
-        publisher, tests = publisher_and_test_level(groups)
-        return {'publisher':publisher, 
+        organisation, tests = organisation_and_test_level(groups)
+        return {'organisation':organisation, 
                 'tests': tests, 
                 'operation': 0, 
                 'condition': 'activity hierarchy', 
@@ -68,7 +68,7 @@ def parsePC(publisher_structures):
     test_functions = {}
     comment = re.compile('#')
     blank = re.compile('^$')
-    for n, line in publisher_structures.items():
+    for n, line in organisation_structures.items():
         line = line.strip('\n')
         number = n
 
@@ -87,5 +87,5 @@ def parsePC(publisher_structures):
     return test_functions
 
 if __name__ == '__main__':
-    publisher_structures = {"GB-1 does not use document-link at activity hierarchy 2", "44000 does not use default-tied-status at activity level"}
-    print parsePC(publisher_structures)
+    organisation_structures = {"GB-1 does not use document-link at activity hierarchy 2", "44000 does not use default-tied-status at activity level"}
+    print parsePC(organisation_structures)
