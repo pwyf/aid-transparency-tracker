b5babf83e536e77e84679b487074f303792212a7
Martin Keegan <martin@no.ucant.org>
2013-04-24 15:28:57 +0100

    hack iterators back in

--- iatidq/summary.py
+++ iatidq/summary.py
@@ -230,14 +230,14 @@ def _agr_results(data, conditions=None, mode=None):
 
     def summaries(summary_f):
         for h, t in itertools.product(hierarchies, tests):
-            yield summary_f(h, t)
+            yield h, t, summary_f(h, t)
 
     if mode in ["publisher", "publisher_simple", "publisher_indicators"]:
         summary = sum_for_publishers
     else:
         summary = sum_default
 
-    for tdata in summaries(summary):
+    for h, t, tdata in summaries(summary):
         if h not in out:
             out[h] = {}
         if t not in out[h]:
