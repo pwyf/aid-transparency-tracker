0674f8a79b3f5a81473e5ab17ff3ca8148fd3dc2
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-04-26 12:07:12 +0100

    Added some helper functions for dqpackages

--- iatidq/dqpackages.py
+++ iatidq/dqpackages.py
@@ -13,8 +13,26 @@ import models
 def package_status(package_id):
     return models.PackageStatus.query.filter_by(package_id=package_id).order_by("runtime_datetime desc").first()
 
-def packages():
-    return models.Package.query.order_by(models.Package.package_name).all()
+def packages(package_id=None):
+    if id is not None:
+        return models.Package.query.filter_by(id=package_id).order_by(models.Package.package_name).first()
+    else:
+        return models.Package.query.order_by(models.Package.package_name).all()
+
+def packages_by_name(package_name):
+    return models.Package.query.filter_by(package_name=package_name).order_by(models.Package.package_name).first()
+
+def packages_by_packagegroup(packagegroup_id=None):
+    return models.Package.query.filter(models.PackageGroup.id==packagegroup_id
+                ).join(models.PackageGroup
+                ).order_by(models.Package.package_name
+                ).all()    
+
+def packages_by_packagegroup_name(packagegroup_name=None):
+    return models.Package.query.filter(models.PackageGroup.name==packagegroup_name
+                ).join(models.PackageGroup
+                ).order_by(models.Package.package_name
+                ).all()    
 
 def packageGroups():
     return models.PackageGroup.query.order_by(models.PackageGroup.name).all()
