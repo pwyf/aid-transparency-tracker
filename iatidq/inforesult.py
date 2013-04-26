da70547cbad1e2bad6fb68a2cf27ad560a4e64dc
Martin Keegan <martin@no.ucant.org>
2013-04-26 10:05:00 +0100

    add inforesult file

---
+++ iatidq/inforesult.py
@@ -0,0 +1,15 @@
+
+#  IATI Data Quality, tools for Data QA on IATI-formatted  publications
+#  by Mark Brough, Martin Keegan, Ben Webb and Jennifer Smith
+#
+#  Copyright (C) 2013  Publish What You Fund
+#
+#  This programme is free software; you may redistribute and/or modify
+#  it under the terms of the GNU Affero General Public License v3.0
+
+def infotest1(filename):
+    return "result1"
+
+
+def infotest2(filename):
+    return "result2"
