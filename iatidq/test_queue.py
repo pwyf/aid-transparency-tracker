e66143a92a8e305d6e7e651d368c2d183136b2c1
Martin Keegan <martin@no.ucant.org>
2013-04-27 12:04:32 +0100

    remove duplicate variable

--- iatidq/test_queue.py
+++ iatidq/test_queue.py
@@ -27,7 +27,6 @@ class InvalidXPath(Exception): pass
 def get_organisations_for_testing(package_id):
     organisations = []
     conditions = []
-    conditions_specified = False
     packageorganisations = dqpackages.packageOrganisations(package_id)
 
     if packageorganisations:
@@ -37,7 +36,6 @@ def get_organisations_for_testing(package_id):
             condition = packageorganisation.OrganisationPackage.condition
             if condition is not None:
                 condition = "[" + condition.strip() + "]"
-                conditions_specified = True
                 conditions.append(condition)
             else:
                 condition = ""
@@ -50,7 +48,7 @@ def get_organisations_for_testing(package_id):
         conditions_str = " or ".join(conditions)
         remainder_xpath = "//iati-activity[not(%s)]" % conditions_str
 
-        if conditions_specified:
+        if conditions:
             organisations.append({
                     'organisation_id': None,
                     'activities_xpath': remainder_xpath
