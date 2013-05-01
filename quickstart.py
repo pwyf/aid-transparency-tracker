b8f09b57289a5ae1749a33fa9141bb4b3fb67df9
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-01 18:27:07 +0100

    Add an organisation package to quickstart

--- quickstart.py
+++ quickstart.py
@@ -38,7 +38,8 @@ which_packages = [
     (u'worldbank-tz', True),
     (u'unops-tz', True),
     (u'dfid-tz', True),
-    (u'unitedstates-tz', True)
+    (u'unitedstates-tz', True),
+    (u'dfid-org', True)
     ]
 
 def refresh(options):
