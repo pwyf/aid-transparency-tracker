3e1bd1498154968faddf9c2b2900a4adb06bca8c
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-04 12:26:03 +0100

    Remove redundant function

--- iatidataquality/routes.py
+++ iatidataquality/routes.py
@@ -77,12 +77,6 @@ def login():
             flash(u"Invalid username.", "error")
     return render_template("login.html")
 
-@app.route("/aggregate_results/<package_id>/<runtime>")
-def display_aggregate_results(package_id, runtime):
-    dqprocessing.aggregate_results(runtime, package_id)
-    db.session.commit()
-    return "ok"
-
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
