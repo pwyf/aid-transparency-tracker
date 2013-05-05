4fb6409f38281a5552227ae6b8e494e928564f02
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-05 15:45:53 +0100

    Remove more unnecessary dependencies

--- iatidataquality/aggregationtypes.py
+++ iatidataquality/aggregationtypes.py
@@ -9,32 +9,13 @@
 
 from flask import Flask, render_template, flash, request, Markup, \
     session, redirect, url_for, escape, Response, abort, send_file
-import StringIO
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUser,
-                            confirm_login, fresh_login_required)
-from flask.ext.principal import identity_loaded, Permission, RoleNeed, \
-     UserNeed
-from sqlalchemy import func
-from datetime import datetime
 
 from iatidataquality import app
 from iatidataquality import db
 import usermanagement
 
-import os
-import sys
-import json
-
-current = os.path.dirname(os.path.abspath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
-
 from iatidq import dqtests, dqaggregationtypes
-
-import StringIO
-import unicodecsv
 
 @app.route("/aggregationtypes/")
 @app.route("/aggregationtypes/<aggregationtype_id>/")
