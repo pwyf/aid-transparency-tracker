8f82d4cee63adf7773c6292c587a75db1e52087f
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-06 02:44:49 +0100

    Get noformat and ordinal from summary

--- iatidq/summary.py
+++ iatidq/summary.py
@@ -53,7 +53,9 @@ def publisher_indicators(indicators, indicators_tests, simple_out):
                 "indicator_type": indicatordata[2],
                 "indicator_category_name": indicatordata[3],
                 "indicator_subcategory_name": indicatordata[4],
-                "longdescription": indicatordata[5]
+                "longdescription": indicatordata[5], 
+                "indicator_noformat": indicatordata[6],
+                "indicator_ordinal": indicatordata[7]
                 },
             "tests": indicator_test_data,
             "results_pct": (results_weighted_pct_average_numerator/results_num),
@@ -265,7 +267,7 @@ def _agr_results(data, conditions=None, mode=None):
         return dict(map(lam, data))
 
     if publisher_mode(mode):
-        ind_f = lambda x: (x[0]["id"], (x[0]["name"], x[0]["description"], x[0]["indicator_type"], x[0]["indicator_category_name"], x[0]["indicator_subcategory_name"], x[0]["longdescription"]))
+        ind_f = lambda x: (x[0]["id"], (x[0]["name"], x[0]["description"], x[0]["indicator_type"], x[0]["indicator_category_name"], x[0]["indicator_subcategory_name"], x[0]["longdescription"], x[0]["indicator_noformat"], x[0]["indicator_ordinal"]))
         indicators = setmap(ind_f)
 
         ind_test_f = lambda x: (x[0]["id"], x[1].id)
