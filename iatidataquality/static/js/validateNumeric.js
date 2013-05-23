ff5208b9dae6d2f9310109ff87018717970a44a4
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-23 15:36:43 +0100

    Updated button

--- iatidataquality/static/js/validateNumeric.js
+++ iatidataquality/static/js/validateNumeric.js
@@ -12,7 +12,7 @@ $("form").submit(function(e){
     if (errors) {
         e.preventDefault();
         if (!$('#dataConfirmModal').length) {
-            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Errors in your survey input</h3></div><div class="modal-body"></div><div class="modal-footer"><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
+            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Errors in your survey input</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">OK</button></div></div>');
         }
         warning_text = "Commitment indicators must have numerical scores. Please correct errors above.";
         $('#dataConfirmModal').find('.modal-body').text(warning_text);
