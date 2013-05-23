62397025fb93c6732ccf20624eefeb71ed0eaee0
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-23 16:49:18 +0100

    Use correct Bootstrap hide call for modal

--- iatidataquality/static/js/validateNumeric.js
+++ iatidataquality/static/js/validateNumeric.js
@@ -11,7 +11,7 @@ $("form").submit(function(e){
     });
     if (errors) {
         e.preventDefault();
-        $('#dataConfirmModal').modal({show:false});
+        $('#dataConfirmModal').modal('hide');
         if (!$('#dataConfirmModal').length) {
             $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Errors in your survey input</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">OK</button></div></div>');
         }
