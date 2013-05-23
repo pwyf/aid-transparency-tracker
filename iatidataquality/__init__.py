e2793c1fc9db6df77503ecac1aa067c520f166a5
Martin Keegan <martin@no.ucant.org>
2013-05-23 13:41:27 +0100

    fix config lookup

--- iatidataquality/__init__.py
+++ iatidataquality/__init__.py
@@ -12,7 +12,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__.split('.')[0])
-app.config.from_pyfile(os.path.join('..', '/config.py'))
+app.config.from_pyfile(os.path.join('..', 'config.py'))
 db = SQLAlchemy(app)
 
 import usermanagement
