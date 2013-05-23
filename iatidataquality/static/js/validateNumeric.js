a186084b3e13720520649b9bc99cbb367c7eadc4
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-05-23 16:51:03 +0100

    Remove name conflict

--- iatidataquality/static/js/validateNumeric.js
+++ iatidataquality/static/js/validateNumeric.js
@@ -12,12 +12,12 @@ $("form").submit(function(e){
     if (errors) {
         e.preventDefault();
         $('#dataConfirmModal').modal('hide');
-        if (!$('#dataConfirmModal').length) {
-            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Errors in your survey input</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">OK</button></div></div>');
+        if (!$('#commitmentWarning').length) {
+            $('body').append('<div id="commitmentWarning" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Errors in your survey input</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">OK</button></div></div>');
         }
         warning_text = "Commitment indicators must have numerical scores. Please correct errors above.";
-        $('#dataConfirmModal').find('.modal-body').text(warning_text);
-        $('#dataConfirmModal').modal({show:true});
+        $('#commitmentWarning').find('.modal-body').text(warning_text);
+        $('#commitmentWarning').modal({show:true});
     }
 });
 
