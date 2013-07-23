e5f34ad06ef1f2eac32c79dc0b15b619831107d0
Mark Brough <mark.brough@publishwhatyoufund.org>
2013-07-23 20:42:53 +0100

    Add debugging line

--- iatidataquality/surveys.py
+++ iatidataquality/surveys.py
@@ -262,6 +262,7 @@ def organisation_survey_view(organisation_code, workflow,
     dqsurveys.getSurveyData(organisation_code, workflow_name)
 
     surveydata = dqsurveys.getSurveyDataAllWorkflows(organisation_code)
+    print surveydata
 
     indicators = dqindicators.indicators("2013 Index")
     org_indicators = dqorganisations._organisation_indicators_split(
