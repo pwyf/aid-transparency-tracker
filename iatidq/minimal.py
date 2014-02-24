46b6a6b73e6b69b97e95217c58587cf67f3cd702
Martin Keegan <martin@no.ucant.org>
2014-02-24 11:52:14 +0000

    move minimal org info into same file

--- iatidq/minimal.py
+++ iatidq/minimal.py
@@ -7,3 +7,90 @@ which_packages = [
     (u'worldbank-789', True),
     (u'worldbank-ao', True),
     ]
+
+default_minimal_organisations = [
+            {
+            'organisation_name': 'World Bank',
+            'organisation_code': '44002',
+            'packagegroup_name': 'worldbank',
+            'condition': None,
+            'package_name': 'worldbank-tz',
+             "organisation_total_spend": "11703.48",
+             "organisation_total_spend_source": None,
+             "organisation_currency": None,
+             "organisation_currency_conversion": None,
+             "organisation_currency_conversion_source": None,
+             "organisation_largest_recipient": None,
+             "organisation_largest_recipient_source": None
+            },
+            {
+            'organisation_name': 'USAID',
+            'organisation_code': 'US-1',
+            'packagegroup_name': 'unitedstates',
+            'condition': 'participating-org[@role="Extending"][@ref="US-1"]',
+            'package_name': 'unitedstates-tz',
+             "organisation_total_spend": "11324",
+             "organisation_total_spend_source": None,
+             "organisation_currency": None,
+             "organisation_currency_conversion": None,
+             "organisation_currency_conversion_source": None,
+             "organisation_largest_recipient": None,
+             "organisation_largest_recipient_source": None
+            },
+            {
+            'organisation_name': 'MCC',
+            'organisation_code': 'US-18',
+            'packagegroup_name': 'unitedstates',
+            'condition': 'participating-org[@role="Extending"][@ref="US-18"]',
+            'package_name': 'unitedstates-tz',
+             "organisation_total_spend": "1591",
+             "organisation_total_spend_source": None,
+             "organisation_currency": None,
+             "organisation_currency_conversion": None,
+             "organisation_currency_conversion_source": None,
+             "organisation_largest_recipient": None,
+             "organisation_largest_recipient_source": None
+            },
+            {
+            'organisation_name': 'UK, DFID',
+            'organisation_code': 'GB-1',
+            'packagegroup_name': 'dfid',
+            'condition': None,
+            'package_name': 'dfid-tz',
+             "organisation_total_spend": "3750.21",
+             "organisation_total_spend_source": None,
+             "organisation_currency": None,
+             "organisation_currency_conversion": None,
+             "organisation_currency_conversion_source": None,
+             "organisation_largest_recipient": None,
+             "organisation_largest_recipient_source": None
+            },
+            {
+            'organisation_name': 'UK, DFID',
+            'organisation_code': 'GB-1',
+            'packagegroup_name': 'dfid',
+            'condition': None,
+            'package_name': 'dfid-org',
+             "organisation_total_spend": "3750.21",
+             "organisation_total_spend_source": None,
+             "organisation_currency": None,
+             "organisation_currency_conversion": None,
+             "organisation_currency_conversion_source": None,
+             "organisation_largest_recipient": None,
+             "organisation_largest_recipient_source": None
+            },
+            {
+            'organisation_name': 'UK, DFID',
+            'organisation_code': 'GB-1',
+            'packagegroup_name': 'dfid',
+            'condition': None,
+            'package_name': 'dfid-ug',
+             "organisation_total_spend": "3750.21",
+             "organisation_total_spend_source": None,
+             "organisation_currency": None,
+             "organisation_currency_conversion": None,
+             "organisation_currency_conversion_source": None,
+             "organisation_largest_recipient": None,
+             "organisation_largest_recipient_source": None
+            }
+        ]
--- iatidq/setup.py
+++ iatidq/setup.py
@@ -21,98 +21,12 @@ import inforesult
 import iatidq.survey
 import dqusers
 
-from minimal import which_packages
+from minimal import which_packages, default_minimal_organisations
 
 default_tests_filename="tests/tests.csv"
 default_infotypes_filename="tests/infotypes.csv"
 default_indicator_group_name= app.config["INDICATOR_GROUP"]
 default_userdata_filename='tests/users.csv'
-default_minimal_organisations = [
-            {
-            'organisation_name': 'World Bank',
-            'organisation_code': '44002',
-            'packagegroup_name': 'worldbank',
-            'condition': None,
-            'package_name': 'worldbank-tz',
-             "organisation_total_spend": "11703.48",
-             "organisation_total_spend_source": None,
-             "organisation_currency": None,
-             "organisation_currency_conversion": None,
-             "organisation_currency_conversion_source": None,
-             "organisation_largest_recipient": None,
-             "organisation_largest_recipient_source": None
-            },
-            {
-            'organisation_name': 'USAID',
-            'organisation_code': 'US-1',
-            'packagegroup_name': 'unitedstates',
-            'condition': 'participating-org[@role="Extending"][@ref="US-1"]',
-            'package_name': 'unitedstates-tz',
-             "organisation_total_spend": "11324",
-             "organisation_total_spend_source": None,
-             "organisation_currency": None,
-             "organisation_currency_conversion": None,
-             "organisation_currency_conversion_source": None,
-             "organisation_largest_recipient": None,
-             "organisation_largest_recipient_source": None
-            },
-            {
-            'organisation_name': 'MCC',
-            'organisation_code': 'US-18',
-            'packagegroup_name': 'unitedstates',
-            'condition': 'participating-org[@role="Extending"][@ref="US-18"]',
-            'package_name': 'unitedstates-tz',
-             "organisation_total_spend": "1591",
-             "organisation_total_spend_source": None,
-             "organisation_currency": None,
-             "organisation_currency_conversion": None,
-             "organisation_currency_conversion_source": None,
-             "organisation_largest_recipient": None,
-             "organisation_largest_recipient_source": None
-            },
-            {
-            'organisation_name': 'UK, DFID',
-            'organisation_code': 'GB-1',
-            'packagegroup_name': 'dfid',
-            'condition': None,
-            'package_name': 'dfid-tz',
-             "organisation_total_spend": "3750.21",
-             "organisation_total_spend_source": None,
-             "organisation_currency": None,
-             "organisation_currency_conversion": None,
-             "organisation_currency_conversion_source": None,
-             "organisation_largest_recipient": None,
-             "organisation_largest_recipient_source": None
-            },
-            {
-            'organisation_name': 'UK, DFID',
-            'organisation_code': 'GB-1',
-            'packagegroup_name': 'dfid',
-            'condition': None,
-            'package_name': 'dfid-org',
-             "organisation_total_spend": "3750.21",
-             "organisation_total_spend_source": None,
-             "organisation_currency": None,
-             "organisation_currency_conversion": None,
-             "organisation_currency_conversion_source": None,
-             "organisation_largest_recipient": None,
-             "organisation_largest_recipient_source": None
-            },
-            {
-            'organisation_name': 'UK, DFID',
-            'organisation_code': 'GB-1',
-            'packagegroup_name': 'dfid',
-            'condition': None,
-            'package_name': 'dfid-ug',
-             "organisation_total_spend": "3750.21",
-             "organisation_total_spend_source": None,
-             "organisation_currency": None,
-             "organisation_currency_conversion": None,
-             "organisation_currency_conversion_source": None,
-             "organisation_largest_recipient": None,
-             "organisation_largest_recipient_source": None
-            }
-        ]
 
 def create_inforesult_types(options):
     print "Adding info result types"
