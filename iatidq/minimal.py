e9c46c057bf537424d43f10bfad31960013dcf8c
Martin Keegan <martin@no.ucant.org>
2014-02-24 11:52:45 +0000

    rm wrongish msg

--- iatidq/minimal.py
+++ iatidq/minimal.py
@@ -1,5 +1,12 @@
 
-# FIXME: duplicated
+#  IATI Data Quality, tools for Data QA on IATI-formatted  publications
+#  by Mark Brough, Martin Keegan, Ben Webb and Jennifer Smith
+#
+#  Copyright (C) 2014  Publish What You Fund
+#
+#  This programme is free software; you may redistribute and/or modify
+#  it under the terms of the GNU Affero General Public License v3.0
+
 which_packages = [
     (u'dfid-ml', True),
     (u'dfid-589', True),
