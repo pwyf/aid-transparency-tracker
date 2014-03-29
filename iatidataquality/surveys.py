0a31de016f416609996ee842520a0036a77fcd14
Martin Keegan <martin@no.ucant.org>
2014-03-29 13:23:29 +0000

    move survey mapping into own module

--- iatidataquality/surveys.py
+++ iatidataquality/surveys.py
@@ -16,16 +16,6 @@ from iatidataquality import db
 
 import os
 import sys
-import unicodecsv
-
-YEAR_MAP_FILE = '2012_2013_organisation_mapping.csv'
-OLD_YEAR = '2012'
-NEW_YEAR = '2013'
-OLD_FIELD_ID = OLD_YEAR + '_id'
-NEW_FIELD_ID = NEW_YEAR + '_id'
-OLD_INDICATORS_FILE = OLD_YEAR + '_indicators.csv'
-OLD_RESULTS_FILE = OLD_YEAR + '_results.csv'
-NEW_INDICATOR_NAME = NEW_YEAR + '_indicator_name'
 
 current = os.path.dirname(os.path.abspath(__file__))
 parent = os.path.dirname(current)
@@ -33,6 +23,7 @@ sys.path.append(parent)
 
 from iatidq import dqindicators, dqorganisations, dqusers, donorresponse
 import iatidq.survey.data as dqsurveys
+import iatidq.survey.mapping
 from iatidq.models import *
 
 import usermanagement
@@ -53,65 +44,6 @@ def surveys_admin():
 @app.route("/surveys/<organisation_code>/create/", methods=["GET", "POST"])
 def create_survey(organisation_code=None):
     return "You're trying to create a survey"
-
-def get_old_organisation_id(organisation_code='GB-1'):
-    path = app.config["DATA_STORAGE_DIR"]
-    old_organisation_file = os.path.join(path, YEAR_MAP_FILE)
-
-    old_organisation_data = unicodecsv.DictReader(file(old_organisation_file))
-    for row in old_organisation_data:
-        if row[NEW_FIELD_ID] == organisation_code:
-            return row[OLD_FIELD_ID]
-
-def get_old_indicators():
-    path = app.config["DATA_STORAGE_DIR"]
-    old_indicators_file = os.path.join(path, OLD_INDICATORS_FILE)
-    old_indicators_data = unicodecsv.DictReader(file(old_indicators_file))
-
-    indicator_data = {}
-    for row in old_indicators_data:
-        if ((row["question_number"]) and (row[NEW_INDICATOR_NAME])):
-            indicator_data[int(row["question_number"])] = row[NEW_INDICATOR_NAME]
-    return indicator_data
-
-# read in(!) a CSV file of last year's results; it must have fields
-
-# id              - not used
-# question_id     - the indicator id of the question in the old data
-# target_id       - the organisation ID in the old data
-# result          - used in templates
-# result_evidence - used in templates
-# result_comments - used in templates
-# result_review   - not used
-
-    
-def get_organisation_results(organisation_code, newindicators):
-    old_organisation_id = get_old_organisation_id(organisation_code)
-    indicators = get_old_indicators()
-
-    path = app.config["DATA_STORAGE_DIR"]
-
-    old_results_file = os.path.join(path, OLD_RESULTS_FILE)
-    old_results_data = unicodecsv.DictReader(file(old_results_file))
-
-    data = {}
-
-    for d in old_results_data:
-        if d["target_id"] == old_organisation_id:
-            try:
-                question_id = int(d["question_id"])
-                d["newindicator_id"] = indicators[question_id]
-                data[indicators[question_id]] = d
-            except KeyError:
-                pass
-    for indicator_name in newindicators:
-        try:
-            discard = data[indicator_name]
-        except KeyError:
-            data[indicator_name] = {
-                'result': ''
-            }
-    return data
 
 def completion_percentage(survey):
     stages = ['researcher', 'send', 'donorreview', 'pwyfreview',
@@ -297,7 +229,7 @@ def organisation_survey_view(organisation_code, workflow,
     org_indicators = dqorganisations._organisation_indicators_split(
         organisation, 2)
 
-    twentytwelvedata=get_organisation_results(
+    twentytwelvedata = iatidq.survey.mapping.get_organisation_results(
         organisation_code, 
         [i[1]["indicator"]["name"] for i in org_indicators["zero"].items()]
         )
