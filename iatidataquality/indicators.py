e47b688d22790fff75a89cde2d09901318e19e50
Mark Brough <mark.brough@publishwhatyoufund.org>
2014-04-08 17:51:02 +0100

    Add tests below indicators

--- iatidataquality/indicators.py
+++ iatidataquality/indicators.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 from iatidataquality import app
 from iatidataquality import db
-from iatidq import dqusers
+from iatidq import dqusers, util
 import usermanagement
 
 import os
@@ -110,11 +110,22 @@ def indicators_comparison(indicatorgroup, indicator):
 
 @app.route("/indicators/<indicatorgroup>/")
 def indicators(indicatorgroup=None):
-    indicators = dqindicators.indicators(indicatorgroup)
+    its = {}
+    indicators = dqindicators.indicatorsTests(indicatorgroup)
+    for indicator in indicators:
+        if indicator.Indicator.id not in its:
+            its[indicator.Indicator.id] = {}
+        its[indicator.Indicator.id].update(indicator.Indicator.as_dict())
+        if indicator.Test:
+            if ('tests' not in its[indicator.Indicator.id]):
+                its[indicator.Indicator.id]['tests'] = []
+            its[indicator.Indicator.id]['tests'].append(indicator.Test.as_dict())
+
+    its = util.resort_indicator_tests(its)
     indicatorgroup = dqindicators.indicatorGroups(indicatorgroup)
     return render_template("indicators.html", 
                         indicatorgroup=indicatorgroup, 
-                        indicators=indicators,
+                        indicators=its,
                         admin=usermanagement.check_perms('admin'),
                         loggedinuser=current_user)
 
