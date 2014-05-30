9949af4be79cbcbca5a8951846b84b8d1d4a99f2
Martin Keegan <martin@no.ucant.org>
2014-05-30 16:48:55 +0100

    stash updates

--- sampling/sampling.py
+++ sampling/sampling.py
@@ -4,6 +4,7 @@ import sys
 import psycopg2
 import requests
 import random
+import lxml.etree
 
 def save_url(url, filename):
     resp = requests.get(url)
@@ -16,10 +17,16 @@ def query(*args):
     c.execute(*args)
     return c.fetchall()
 
+
 class SampleOrgTest(object):
     def __init__(self, organisation_id, test_id):
         self.org_id = organisation_id
         self.test_id = test_id
+
+        if self.qualifies():
+            self.activities = self.activity_ids()
+        else:
+            self.activities = []
 
     def qualifies(self):
         rows = query('''select result_data from result 
@@ -30,13 +37,27 @@ class SampleOrgTest(object):
         return len(rows) >= 1
         
     def activity_ids(self):
-        rows = query('''select result_identifier from result 
+        rows = query('''select result_identifier, package_name from result
+                          left join package on result.package_id = package.id
                           where organisation_id = %s 
                             and test_id = %s 
                             and result_data != 0''', 
                      [self.org_id, self.test_id])
-        ids = [ i[0] for i in rows ]
+        ids = [ i for i in rows ]
         return ids
 
-    def sample_activity_ids(self):
-        return random.choice(self.activity_ids())
+    def sample_activity_ids(self, max):
+        act_ids = [i for i in self.activities]
+        random.shuffle(act_ids)
+        return act_ids[:max]
+
+    def xml_of_package(self, package_name):
+        ROOT= '/var/tmp/iati'
+        return lxml.etree.parse(ROOT + '/' + package_name + '.xml')
+
+    def xml_of_activity(self, activity):
+        activity_id, pkg = activity
+        xml = self.xml_of_package(pkg)
+        activities = xml.xpath('//iati-activity[iati-identifier/text()="%s"]' % activity_id)
+        assert len(activities) == 1
+        return lxml.etree.tostring(activities[0], pretty_print=True)
