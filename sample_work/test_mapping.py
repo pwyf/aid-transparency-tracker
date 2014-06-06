725da9749b39325994403ebc3a00912d6f36f822
markbrough <mark.brough@publishwhatyoufund.org>
2014-06-06 18:12:38 +0100

    Add kind to status mapping

--- sample_work/test_mapping.py
+++ sample_work/test_mapping.py
@@ -18,3 +18,93 @@ test_to_kind = {
 	"location exists (if activity-status/@code is at least 2 and recipient-region/@code is not 998)?": "location",
 	"location/coordinates exists (if activity-status/@code is at least 2 and recipient-region/@code is not 998)?": "location"
 }
+
+## Integers 2, 3, 4, etc. Are the response integer that goes into
+## sample_work_item table.
+
+kind_to_status = {
+    "document": [
+        1:
+            {
+              "positive:": "pass",
+              "button": "pass",
+              "icon": "ok",
+              "class": "success",
+            },
+        2:
+            {
+              "positive:": "able to access",
+              "button": "unable to access",
+              "icon": "remove",
+              "class": "danger",
+            },
+        3: 
+            {
+              "positive:": "specific to the activity",
+              "button": "not specific to the activity",
+              "icon": "remove",
+              "class": "danger",
+            },
+        4: 
+            {
+              "positive:": "satisfies the definition",
+              "button": "doesn't satisfy the definition",
+              "icon": "remove",
+              "class": "danger",
+            },
+    ],
+    "result": [
+        2:
+            {
+              "positive:": "contains results information",
+              "button": "doesn't contain results information",
+              "icon": "remove",
+              "class": "danger",
+            },
+        3: 
+            {
+              "positive:": "satisfies the definition",
+              "button": "doesn't satisfy the definition",
+              "icon": "remove",
+              "class": "danger",
+            },
+    ],
+    "conditions": [
+        2:
+            {
+              "positive:": "contains conditions information",
+              "button": "doesn't contain conditions information",
+              "icon": "remove",
+              "class": "danger",
+            },
+        3: 
+            {
+              "positive:": "satisfies the definition",
+              "button": "doesn't satisfy the definition",
+              "icon": "remove",
+              "class": "danger",
+            },
+    ],
+    "location": [
+        2:
+            {
+              "positive:": "contains location information",
+              "button": "doesn't contain location information",
+              "icon": "remove",
+              "class": "danger",
+            },
+        3: 
+            {
+              "positive:": "consistent with documentation",
+              "button": "not consistent with documentation",
+              "icon": "remove",
+              "class": "danger",
+            },
+    ],
+}
+    
+"""
+org-document 	able to access 	unable to access
+org-document 	satisfies the definition 	doesn't satisfy the definition
+org-document 	document is current 	document isn't current
+"""
