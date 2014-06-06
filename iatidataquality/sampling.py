90a43e302aff1aab3c36fd0f5519d527a597cf7a
Martin Keegan <martin@no.ucant.org>
2014-06-06 17:05:38 +0100

    fix syntax, having been confused by massively long lines

--- iatidataquality/sampling.py
+++ iatidataquality/sampling.py
@@ -96,7 +96,8 @@ def make_sample_json(work_item):
     work_item_indicator = get_test_indicator_info(work_item["test_id"])
     work_item_org = get_org_info(work_item["organisation_id"])
 
-    xml = lxml.etree.tostring(lxml.etree.fromstring(work_item['xml_data']), method='xml', pretty_print=True
+    xml = lxml.etree.tostring(lxml.etree.fromstring(work_item['xml_data']), 
+                              method='xml', pretty_print=True)
 
     return { "sample": {
                 "iati-identifier": work_item["activity_id"],
@@ -108,7 +109,7 @@ def make_sample_json(work_item):
                 "activity_title": activity_info.title,
                 "activity_description": activity_info.description,
                 "test_kind": work_item["test_kind"],
-                "xml": xml),
+                "xml": xml,
             },
             "headers": {
                 "test_name": work_item_test.name,
