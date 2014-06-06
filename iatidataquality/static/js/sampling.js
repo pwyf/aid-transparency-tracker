97b1dbce5aad9690c94c8f7c0bc5f622d0ce0878
Martin Keegan <martin@no.ucant.org>
2014-06-06 16:58:29 +0100

    break location setup into own function

--- iatidataquality/static/js/sampling.js
+++ iatidataquality/static/js/sampling.js
@@ -26,6 +26,58 @@ var baseUrl = function() {
 	return url;
 }
 
+var setupLocation = function() {
+    if (typeof(map)!='undefined') {
+        map.remove();
+    }
+	locationx += 2;
+	var geojsonMarkerOptions = {
+	    radius: 8,
+	    fillColor: "#ff7800",
+	    color: "#000",
+	    weight: 1,
+	    opacity: 1,
+	    fillOpacity: 0.8
+	};
+	
+    
+	var locations = survey_data['sample']['locations'];
+
+	markers = new L.MarkerClusterGroup();
+	for (var i in locations) {
+		var feature = locations[i];
+		var marker = new L.Marker(
+			new L.LatLng(
+				feature['latitude'],
+				feature['longitude']
+			    )
+		);
+		var popupContent = '<dl>Location: '+feature['name']+'</dl>';
+		marker.bindPopup(popupContent);
+		markers.addLayer(marker);
+	}
+	
+	// OSM: http://{s}.tile.osm.org/{z}/{x}/{y}.png
+	// MB: http://{s}.tiles.mapbox.com/v3/markbrough.map-qmxr8jb5/{z}/{x}/{y}.png
+	// ARC: https://d.tiles.mapbox.com/v3/americanredcross.map-ms6tihx6/{z}/{x}/{y}.png
+	
+	layer_MapBox = new L.tileLayer(
+	    'https://d.tiles.mapbox.com/v3/americanredcross.map-ms6tihx6/{z}/{x}/{y}.png',{
+			maxZoom: 18, attribution: 'Map data <a href="http://mapbox.com">MapBox Streets</a>'
+	    }
+	)
+    
+    map = new L.Map('projectMap', {
+        zoom: 5,
+        maxZoom: 15
+    });
+	layer_MapBox.addTo(map);
+    map.addLayer(markers);
+    map.fitBounds(markers.getBounds());
+	
+    $("#location-xml").text(vkbeautify.xml($("#location-xml").text()));
+};
+
 var setupNewSurveyForm = function(survey_data) {
 
 	var kind = survey_data['sample']['test_kind'];
@@ -59,56 +111,8 @@ var setupNewSurveyForm = function(survey_data) {
 	var rendered_buttons = Mustache.render(buttons_template, buttons_data);
 	$('#buttons-insert').html(rendered_buttons);
 
-	if (kind=='location'){
-        if (typeof(map)!='undefined') {
-            map.remove();
-        }
-	    locationx += 2;
-	    var geojsonMarkerOptions = {
-	      radius: 8,
-	      fillColor: "#ff7800",
-	      color: "#000",
-	      weight: 1,
-	      opacity: 1,
-	      fillOpacity: 0.8
-	    };
-
-        
-	    var locations = survey_data['sample']['locations'];
-
-	    markers = new L.MarkerClusterGroup();
-	    for (var i in locations) {
-		    var feature = locations[i];
-		    var marker = new L.Marker(
-			    new L.LatLng(
-				    feature['latitude'],
-				    feature['longitude']
-			    )
-		    );
-		    var popupContent = '<dl>Location: '+feature['name']+'</dl>';
-		    marker.bindPopup(popupContent);
-		    markers.addLayer(marker);
-	    }
-
-	    // OSM: http://{s}.tile.osm.org/{z}/{x}/{y}.png
-	    // MB: http://{s}.tiles.mapbox.com/v3/markbrough.map-qmxr8jb5/{z}/{x}/{y}.png
-	    // ARC: https://d.tiles.mapbox.com/v3/americanredcross.map-ms6tihx6/{z}/{x}/{y}.png
-
-	    layer_MapBox = new L.tileLayer(
-	    'https://d.tiles.mapbox.com/v3/americanredcross.map-ms6tihx6/{z}/{x}/{y}.png',{
-	      maxZoom: 18, attribution: 'Map data <a href="http://mapbox.com">MapBox Streets</a>'
-	    }
-	    )
-        
-        map = new L.Map('projectMap', {
-          zoom: 5,
-          maxZoom: 15
-        });
-	    layer_MapBox.addTo(map);
-        map.addLayer(markers);
-        map.fitBounds(markers.getBounds());
-
-        $("#location-xml").text(vkbeautify.xml($("#location-xml").text()));
+	if (kind=='location') {
+		setupLocation();
 	}
 };
 
