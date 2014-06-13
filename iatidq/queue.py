a76748c03205ef4abd0a9cf64f243252133c0cdb
Martin Keegan <martin@no.ucant.org>
2014-06-13 11:09:54 +0100

    set params right

--- iatidq/queue.py
+++ iatidq/queue.py
@@ -56,8 +56,8 @@ def handle_queue_generator(queue_name):
     try:
         connection = get_connection('localhost')
         channel = connection.channel()
-        #channel.queue_declare(queue=queue_name, durable=True)
-        #channel.basic_qos(prefetch_count=1)
+        channel.queue_declare(queue=queue_name, durable=True)
+        channel.basic_qos(prefetch_count=1)
         for method, properties, body in channel.basic_get(queue_name):
             if not method_frame:
                 break
