f2fd0bc423120c8befed35b6c5e568aa951975cf
Martin Keegan <martin@no.ucant.org>
2014-07-24 14:52:48 +0100

    tighten constraint

--- iatidq/models.py
+++ iatidq/models.py
@@ -361,7 +361,7 @@ class IndicatorTest(db.Model):
     id = Column(Integer, primary_key=True)
     indicator_id = Column(Integer, ForeignKey('indicator.id'), nullable=False)
     test_id = Column(Integer, ForeignKey('test.id'), nullable=False)
-    __table_args__ = (UniqueConstraint('indicator_id', 'test_id'), )
+    __table_args__ = (UniqueConstraint('test_id'), )
 
     def setup(self,
                  indicator_id,
