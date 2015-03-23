466770eef0a17c6c14f5a2e2c96fb23fab467809
Mark Brough <mark.brough@gmail.com>
2015-03-23 17:26:49 +0100

    Allow querying of aggregation types by name

--- iatidq/dqaggregationtypes.py
+++ iatidq/dqaggregationtypes.py
@@ -23,6 +23,11 @@ def aggregationTypes(aggregationtype_id=None):
     else:
         checkATs = models.AggregationType.query.filter_by(id=aggregationtype_id).first()
     return checkATs
+    
+def aggregationtype_by_name(name):
+    checkAT = db.session.query(models.AggregationType
+                    ).filter(name=name).first()
+    return checkAT
 
 def allAggregationTypes():
     return db.session.query(models.AggregationType).all()
