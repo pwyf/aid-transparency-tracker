58d7994a7535607d5292578cc509a21137f0fccc
Andy Lulham <a.lulham@gmail.com>
2017-09-08 17:29:02 +0100

    Sort out codelist fetching to not use weird repo

--- iatidq/dqcodelists.py
+++ iatidq/dqcodelists.py
@@ -7,14 +7,15 @@
 #  This programme is free software; you may redistribute and/or modify
 #  it under the terms of the GNU Affero General Public License v3.0
 
+from collections import defaultdict
+
+import requests
+import unicodecsv
+
 from iatidq import db, app
 import models
-import unicodecsv
-from collections import defaultdict
-import urllib2
-import lxml.etree
-
 import util
+
 
 CODELIST_API = app.config["CODELIST_API"]
 
@@ -45,7 +46,7 @@ def handle_row(codelist, codelist_url, crow):
     if not codelistcode:
         codelistcode = models.CodelistCode()
     codelistcode.setup(
-        name = crow['name'],
+        name = crow.get('name', ''),
         code = crow['code'],
         codelist_id = codelist.id
         )
@@ -72,55 +73,35 @@ def add_manual_codelist(filename, codelist_name, codelist_description):
     with db.session.begin():
        [ handle_row(codelist, filename, crow) for crow in codelist_data ]
 
-def handle_codelist(codelists_url, row):
+def handle_codelist(codelists_url, codelist_name):
+    codelist_url = '{}json/en/{}.json'.format(CODELIST_API, codelist_name)
+    print(codelist_url)
+
+    codelist_data = requests.get(codelist_url).json()
+
     with db.session.begin():
         codelist = models.Codelist.query.filter(
-            models.Codelist.name==row['name']).first()
+            models.Codelist.name==codelist_name).first()
 
         if not codelist:
             codelist = models.Codelist()
         codelist.setup(
-            name = row['name'],
-            description = row['description']
-            )
+            name=codelist_name,
+            description=codelist_data['metadata']['description']
+        )
         codelist.source = codelists_url
         db.session.add(codelist)
-        
-    codelist_url = (CODELIST_API % ("codelist/" + row['name'] + ".csv"))
-    print codelist_url
-
-    f = urllib2.urlopen(codelist_url)
-
-    data = f.read()
-    if len(data) == 0:
-        print "warning: zero-length codelist at %s" % codelist_url
-        return
-
-    f = urllib2.urlopen(codelist_url)
-
-    codelist_data = unicodecsv.DictReader(f)
 
     with db.session.begin():
-        [ handle_row(codelist, codelist_url, crow) for crow in codelist_data ]
-
-def pretend_xml_is_csv(f):
-    data = f.read()
-    root = lxml.etree.XML(data)
-    
-    for elt in root.findall('codelist'):
-        yield {
-            "name": elt.find('name').text,
-            "description":elt.find('description').text
-            }
+        [handle_row(codelist, codelist_url, crow) for crow in codelist_data['data']]
 
 def importCodelists():
-    codelists_url = (CODELIST_API % ("codelist.xml"))
+    codelists_url = (CODELIST_API + 'codelists.json')
 
-    f = urllib2.urlopen(codelists_url)
+    codelists_data = requests.get(codelists_url).json()
 
-    codelists_data = pretend_xml_is_csv(f)
-
-    [ handle_codelist(codelists_url, row) for row in codelists_data ]
+    for codelist_name in codelists_data:
+        handle_codelist(codelists_url, codelist_name)
 
     print "Imported successfully"
     return True
