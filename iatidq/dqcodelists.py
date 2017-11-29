e2c323700ab5e242f02aebb1d9c9e5da37e6cdf0
Andy Lulham <a.lulham@gmail.com>
2017-11-29 22:46:15 +0000

    Update codelist code to check against v1 and v2

--- iatidq/dqcodelists.py
+++ iatidq/dqcodelists.py
@@ -16,27 +16,32 @@ from iatidataquality import app, db
 from . import models
 
 
-CODELIST_API = app.config["CODELIST_API"]
-
 def generateCodelists():
-    codelists = db.session.query(models.CodelistCode.code,
-                     models.Codelist.name
-                    ).join(models.Codelist).all()
+    codelists = db.session.query(
+        models.CodelistCode.code,
+        models.Codelist.name
+    ).join(models.Codelist).all()
+
     cl = defaultdict(list)
     for code, codelist in codelists:
         cl[codelist].append(code)
     return dict(cl)
 
+
 def generateACodelist(codelist_name):
-    codelist = db.session.query(models.CodelistCode.name,
-                                models.CodelistCode.code
-            ).filter(models.Codelist.name==codelist_name
-            ).join(models.Codelist).all()
+    codelist = db.session.query(
+        models.CodelistCode.name,
+        models.CodelistCode.code
+    ).filter(
+        models.Codelist.name == codelist_name
+    ).join(models.Codelist).all()
     return codelist
+
 
 def reformatCodelist(codelist_name):
     codelist = generateACodelist(codelist_name)
     return dict(map(lambda x: (x.code, x.name), codelist))
+
 
 def handle_row(codelist, codelist_url, crow):
     codelistcode = models.CodelistCode.query.filter_by(
@@ -45,12 +50,13 @@ def handle_row(codelist, codelist_url, crow):
     if not codelistcode:
         codelistcode = models.CodelistCode()
     codelistcode.setup(
-        name = crow.get('name', ''),
-        code = crow['code'],
-        codelist_id = codelist.id
-        )
+        name=crow.get('name', ''),
+        code=crow['code'],
+        codelist_id=codelist.id
+    )
     codelistcode.source = codelist_url
     db.session.add(codelistcode)
+
 
 def add_manual_codelist(filename, codelist_name, codelist_description):
     f = open(filename)
@@ -58,29 +64,34 @@ def add_manual_codelist(filename, codelist_name, codelist_description):
 
     with db.session.begin():
         codelist = models.Codelist.query.filter(
-            models.Codelist.name==codelist_name).first()
+            models.Codelist.name == codelist_name).first()
 
         if not codelist:
             codelist = models.Codelist()
         codelist.setup(
-            name = codelist_name,
-            description = codelist_description
+            name=codelist_name,
+            description=codelist_description
             )
         codelist.source = filename
         db.session.add(codelist)
 
     with db.session.begin():
-       [ handle_row(codelist, filename, crow) for crow in codelist_data ]
+        for crow in codelist_data:
+            handle_row(codelist, filename, crow)
 
-def handle_codelist(codelists_url, codelist_name):
-    codelist_url = '{}json/en/{}.json'.format(CODELIST_API, codelist_name)
+
+def handle_codelist(codelists_url, codelist_url, codelist_name):
     print(codelist_url)
 
     codelist_data = requests.get(codelist_url).json()
+    if codelist_data['attributes']['complete'] != '1':
+        # we don't care about incomplete codelists,
+        # because we can't check against them anyway
+        return
 
     with db.session.begin():
         codelist = models.Codelist.query.filter(
-            models.Codelist.name==codelist_name).first()
+            models.Codelist.name == codelist_name).first()
 
         if not codelist:
             codelist = models.Codelist()
@@ -92,18 +103,29 @@ def handle_codelist(codelists_url, codelist_name):
         db.session.add(codelist)
 
     with db.session.begin():
-        [handle_row(codelist, codelist_url, crow) for crow in codelist_data['data']]
+        for crow in codelist_data['data']:
+            handle_row(codelist, codelist_url, crow)
+
 
 def importCodelists():
-    codelists_url = (CODELIST_API + 'codelists.json')
+    tmpl_prefix = app.config["CODELIST_API"]
 
-    codelists_data = requests.get(codelists_url).json()
+    mapping_tmpl = tmpl_prefix + '/mapping.json'
+    codelist_tmpl = tmpl_prefix + '/json/en/{codelist_name}.json'
+    versions = ('1.05', '2.02',)
 
-    for codelist_name in codelists_data:
-        handle_codelist(codelists_url, codelist_name)
-
-    print "Imported successfully"
+    for version in versions:
+        version = version.replace('.', '')
+        codelists_url = mapping_tmpl.format(version=version)
+        mapping = requests.get(codelists_url).json()
+        codelist_names = {x['codelist']: None for x in mapping}.keys()
+        for codelist_name in codelist_names:
+            codelist_url = codelist_tmpl.format(
+                version=version, codelist_name=codelist_name)
+            handle_codelist(codelists_url, codelist_url, codelist_name)
+    print("Imported successfully")
     return True
+
 
 if __name__ == "__main__":
     importCodelists()
