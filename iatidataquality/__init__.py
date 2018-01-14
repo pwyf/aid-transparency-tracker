1f448e403b601e6bda2ec579dd5a8489f28b992f
Andy Lulham <a.lulham@gmail.com>
2018-01-14 14:43:46 +0000

    Move url_for stuff to tmpl_filters.py
    Bit of a weird place for it, I know.

--- iatidataquality/__init__.py
+++ iatidataquality/__init__.py
@@ -9,28 +9,13 @@
 
 import os
 
-from flask import url_for, Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__.split('.')[0])
 app.config.from_pyfile(os.path.join('..', 'config.py'))
 db = SQLAlchemy(app, session_options={"autocommit": True})
-
-
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
 
 
 from . import tmpl_filters
--- iatidataquality/tmpl_filters.py
+++ iatidataquality/tmpl_filters.py
@@ -1,8 +1,24 @@
+import os
 import re
 
-from flask import Markup
+from flask import Markup, url_for
 
 from . import app
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 @app.template_filter('hyperlink')
