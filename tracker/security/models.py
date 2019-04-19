5e7d8bfcb4ea24e4d43764e54583403747938a23
Andy Lulham <a.lulham@gmail.com>
2019-04-19 23:25:47 +0100

    Store some last login info on the user model

--- tracker/security/models.py
+++ tracker/security/models.py
@@ -30,6 +30,10 @@ class User(BaseModel, UserMixin):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(100))
+    current_login_ip = db.Column(db.String(100))
+    login_count = db.Column(db.Integer)
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
