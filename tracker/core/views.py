8c76d2d52e054ece77184b96ed757f4090497f87
Andy Lulham <a.lulham@gmail.com>
2019-04-21 17:53:53 +0100

    Sort out homepage view a bit

--- tracker/core/views.py
+++ tracker/core/views.py
@@ -1,5 +1,7 @@
 """Core views."""
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, redirect, url_for
+from flask_security.decorators import login_required
+from flask_security.core import current_user
 
 from . import models
 
@@ -8,9 +10,17 @@ blueprint = Blueprint('core', __name__, url_prefix='/', static_folder='../static
 
 
 @blueprint.route('/')
+@login_required
 def home():
     """Home page."""
-    organisations = models.Organisation.query
+    if current_user.has_role('admin'):
+        organisations = models.Organisation.query
+    else:
+        organisations = [o for o in models.Organisation.query
+                         if current_user.has_role(organisation_id=o.id)]
+        if len(organisations) == 1:
+            return redirect(url_for('core.summary', org_id=organisations[0].id))
+
     return render_template('core/home.html', organisations=organisations)
 
 
