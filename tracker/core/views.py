340314080609c1f03e55feae80e7b282eeff4303
Andy Lulham <a.lulham@gmail.com>
2019-04-21 13:00:09 +0100

    Add list of orgs to homepage

--- tracker/core/views.py
+++ tracker/core/views.py
@@ -1,6 +1,8 @@
 """Core views."""
 from flask import Blueprint, render_template
 
+from . import models
+
 
 blueprint = Blueprint('core', __name__, url_prefix='/', static_folder='../static')
 
@@ -8,4 +10,5 @@ blueprint = Blueprint('core', __name__, url_prefix='/', static_folder='../static
 @blueprint.route('/')
 def home():
     """Home page."""
-    return render_template('core/home.html')
+    organisations = models.Organisation.query
+    return render_template('core/home.html', organisations=organisations)
