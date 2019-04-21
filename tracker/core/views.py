87bcc875aa89c25179866300b50acb3d7816a987
Andy Lulham <a.lulham@gmail.com>
2019-04-21 17:33:31 +0100

    Consistent language

--- tracker/core/views.py
+++ tracker/core/views.py
@@ -1,5 +1,5 @@
 """Core views."""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 
 from . import models
 
@@ -12,3 +12,13 @@ def home():
     """Home page."""
     organisations = models.Organisation.query
     return render_template('core/home.html', organisations=organisations)
+
+
+@blueprint.route('/summary/<org_id>')
+def summary(org_id):
+    """Summary page."""
+    try:
+        organisation = models.Organisation.find_or_fail(org_id)
+    except:
+        return abort(404)
+    return render_template('core/summary.html', organisation=organisation)
