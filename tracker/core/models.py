7a35e339f2c4f9d1c8f8b10394df59e5a5d50902
Andy Lulham <a.lulham@gmail.com>
2019-04-23 08:30:07 +0100

    Add component and indicator models

--- tracker/core/models.py
+++ tracker/core/models.py
@@ -12,5 +12,26 @@ class Organisation(BaseModel):
     test_condition = db.Column(db.String(255))
 
 
-# class Indicator(BaseModel):
-#     pass
+class Component(BaseModel):
+    """An index methodology component. Indicators are grouped by these."""
+
+    id = db.Column(db.String(255), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+
+
+class Indicator(BaseModel):
+    """An indicator of transparency."""
+
+    id = db.Column(db.String(255), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    indicator_type = db.Column(db.String(255), nullable=False)
+    component_id = db.Column(db.String(255), db.ForeignKey('component.id',
+                             ondelete='CASCADE'), nullable=False)
+    component = db.relationship('Component',
+                                backref=db.backref('indicators', lazy=True))
+    description = db.Column(db.Text(), nullable=False)
+    order = db.Column(db.Integer(), nullable=False)
+    ordinal = db.Column(db.Boolean, nullable=False)
+    weight = db.Column(db.String(255), nullable=False)
+    has_format = db.Column(db.Boolean, nullable=False)
