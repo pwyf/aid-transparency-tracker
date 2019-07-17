9e05123b8d5c3c8384dc20773c5bce2a9bcca466
Jared Parnell <jared.parnell@opendataservices.coop>
2019-07-17 16:07:52 +0100

    Original with vagrant and CSV Tool (#517)
    Includes vagrant setup for local development and a CLI tool for organisation condition bulk upload from CSV.

---
+++ vagrant-readme.md
@@ -0,0 +1,35 @@
+# Run With Vagrant
+
+Make sure disksize plugin is installed:
+
+```
+vagrant plugin install vagrant-disksize
+
+vagrant up
+```
+
+## How to run
+
+``` bash
+vagrant ssh
+
+cd /vagrant
+
+source pyenv/bin/activate
+
+flask download_data
+
+flask import_data
+
+flask test_data
+
+flask aggregate_results
+```
+
+## Run webservice with vagrant
+
+webservice:
+
+``` bash
+flask run --host 0.0.0.0 --port 8080
+```
---
+++ vagrant/config.py
@@ -0,0 +1,43 @@
+from os.path import abspath, dirname, join, realpath
+
+
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_VHOST = "/"
+BROKER_USER = "guest"
+BROKER_PASSWORD = "guest"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2:///vagrant'
+DATABASE_INFO = {
+    'database': 'vagrant'
+}
+
+DATA_STORAGE_DIR = join(realpath(dirname(__file__)), 'dq', 'data')
+SAMPLING_DB_FILENAME = join(realpath(dirname(__file__)), 'dq', 'sample_work', 'sample_work.db')
+
+SECRET_KEY = "secret"
+
+INDICATOR_GROUP = u"2018index"
+
+CODELIST_API = u"https://reference.iatistandard.org/{version}/codelists/downloads/clv2"
+
+SETUP_ORGS = ['dfid', 'fco', 'beis', 'dhsc', 'defra_transparency', 'dwp', 'ukmod']
+
+# SETUP_PKG_COUNTER = 10
+
+ORG_FREQUENCY_API_URL = "http://dashboard.iatistandard.org/timeliness_frequency.csv"
+IATIUPDATES_URL = "http://2018tracker.publishwhatyoufund.org/iatiupdates/api/package/hash/"
+
+REMOVE_RESULTS = True
+
+INTRO_HTML = ''
+# INTRO_HTML = 'Data collection for the <a href="http://www.publishwhatyoufund.org/the-index/">2018 Aid Transparency Index</a> has now started. We will release more detailed information in June 2018 when the Aid Transparency Index will be launched. Results and analysis for previous years is available in the <a href="http://ati.publishwhatyoufund.org/" target="_blank">2016 Index</a>.'
+
+ATI_YEAR = "2019"
+PREVIOUS_ATI_YEAR = "2018"
+
+basedir = dirname(abspath(__file__))
+IATI_DATA_PATH = join(basedir, 'dq', 'data')
+IATI_RESULT_PATH = join(basedir, 'dq', 'results')
+
