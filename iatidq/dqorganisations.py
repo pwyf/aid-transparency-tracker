ca6c62f47fff2e0a43dd1c0a42196ca443bb178b
Mark Brough <mark@brough.io>
2026-07-05 00:50:02 +0200

    Fix a couple more inverse badges

--- iatidq/dqorganisations.py
+++ iatidq/dqorganisations.py
@@ -506,7 +506,7 @@ def get_ordinal_values_years():
         },
         0: {
             'text': 'No forward data',
-            'class': 'inverse',
+            'class': 'secondary',
         },
         -1: {
             'text': 'Unknown',
