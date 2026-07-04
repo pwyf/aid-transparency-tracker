38904ceeb05718e11fb7104812c6ecd39642bdca
Mark Brough <mark@brough.io>
2026-07-04 18:22:04 +0200

    Show which conditions have already been applied

--- iatidataquality/organisations_feedback.py
+++ iatidataquality/organisations_feedback.py
@@ -7,11 +7,39 @@
 #  This programme is free software; you may redistribute and/or modify
 #  it under the terms of the GNU Affero General Public License v3.0
 
+import re
+
 from flask import render_template, flash, request, redirect, url_for
 from flask_login import current_user
 
 from . import usermanagement
 from iatidq import dqorganisations, models
+
+
+def _applied_conditions(organisation):
+    from iatidataquality import db
+    rows = db.session.query(
+        models.OrganisationCondition, models.Test
+    ).filter(
+        models.OrganisationCondition.organisation_id == organisation.id
+    ).join(
+        models.Test, models.OrganisationCondition.test_id == models.Test.id
+    ).all()
+
+    lines = set()
+    for oc, test in rows:
+        if not test.name:
+            continue
+        m = re.search(r"Then (?:every )?`([^/`@\[\s]+)", test.name)
+        if not m:
+            continue
+        element = m.group(1)
+        org_code = organisation.organisation_code
+        if oc.condition == 'activity level':
+            lines.add(f"{org_code} does not use {element} at activity level")
+        elif oc.condition == 'activity hierarchy':
+            lines.add(f"{org_code} does not use {element} at activity hierarchy {oc.condition_value}")
+    return sorted(lines)
 
 
 def organisation_feedback(organisation_code=None):
@@ -34,10 +62,12 @@ def organisation_feedback(organisation_code=None):
         existing_feedback = models.OrganisationConditionFeedback.query.filter_by(
             organisation_id=organisation.id
         ).all()
+        applied = _applied_conditions(organisation)
         return render_template(
             "organisation_feedback.html",
             organisation=organisation,
             existing_feedback=existing_feedback,
+            applied_conditions=applied,
             admin=usermanagement.check_perms('admin'),
             loggedinuser=current_user)
     else:
